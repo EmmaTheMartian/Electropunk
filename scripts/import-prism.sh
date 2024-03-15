e5fc5a6a0baa6c8de6585153c97d405122ec691d
EmmaTheMartian <>
2024-03-15 14:40:05 -0400

    Update scripts

---
+++ scripts/import-prism.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+if [ -z "$1" ]; then
+    echo "usage: import-prism.sh (OUTPUT FILE)"
+    echo "example: \`import-prism.sh modrinth\` will import the zip at ./output/exports/modrinth.zip"
+    exit 1
+fi
+
+prismlauncher -I ./output/exports/$1.zip
